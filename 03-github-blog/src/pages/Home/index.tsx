741e3ce563b7141c4c21d755e6a6a41e18e413bb
Guilherme <gibbon309@hotmail.com>
2024-08-18 16:56:59 -0300

    add info in profile

--- 03-github-blog/src/pages/Home/index.tsx
+++ 03-github-blog/src/pages/Home/index.tsx
@@ -1,24 +1,52 @@
+import axios from "axios";
 import { HomeContainer } from "./styles";
+import { useState, useEffect } from "react";
 
+interface TypesProfile {
+  name: string
+  login: string
+  avatar: string
+  followers: string
+}
 export function Home() {
+  const [profile, setProfile] = useState<TypesProfile>()
+
+  async function getProfile () {
+    const response = await axios.get("https://api.github.com/users/gsgibbon")
+
+    const data = response.data;
+
+    setProfile({
+      name: data.name,
+      login: data.login,
+      avatar: data.avatar_url,
+      followers: data.followers
+    })
+  }
+
+  useEffect(() => {
+    getProfile()
+  }, [])
+
   return(
     <HomeContainer>
-      <div>
-        <img src="" alt="" />
+      {profile && 
         <div>
-          <h1>Guilherme Gibbon</h1>
-          <p>
-            Tristique volutpat pulvinar vel massa, 
-            pellentesque egestas. Eu viverra massa quam dignissim
-            aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.
-          </p>
-          <nav>
-            <a href="#">Gsgibbon</a>
-            <a href="#">home</a>
-            <a href="#">0 seguidores</a>
-          </nav>
+          <img src={profile.avatar} alt="" />
+          <div>
+            <h1>{profile.name}</h1>
+            <p>
+              Tristique volutpat pulvinar vel massa, 
+              pellentesque egestas. Eu viverra massa quam dignissim
+              aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.
+            </p>
+            <nav>
+              <a href="#">{profile.login}</a>
+              <a href="#">{profile.followers}</a>
+            </nav>
+          </div>
         </div>
-      </div>
+      }
 
       <div>
         <h3>Publicações</h3>  
