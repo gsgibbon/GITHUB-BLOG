8875f595007d3347812333c651191354e5f62e38
Guilherme <gibbon309@hotmail.com>
2024-08-18 16:16:49 -0300

    add home base

---
+++ 03-github-blog/src/pages/Home/index.tsx
@@ -0,0 +1,35 @@
+export function Home() {
+  return(
+    <main>
+      <div>
+        <img src="" alt="" />
+        <div>
+          <h1>Guilherme Gibbon</h1>
+          <p>Tristique volutpat pulvinar vel massa, 
+            pellentesque egestas. Eu viverra massa quam dignissim
+            aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.
+          </p>
+          <nav>
+            <a href="#">Gsgibbon</a>
+            <a href="#">home</a>
+            <a href="#">0 seguidores</a>
+          </nav>
+        </div>
+      </div>
+
+      <div>
+        <h3>Publicações</h3>  
+        <input type="text" placeholder="Buscar conteúdo"/>
+      </div>
+
+      <div>
+        <div>1</div>  
+        <div>2</div>  
+        <div>3</div>  
+        <div>4</div>  
+        <div>5</div>  
+        <div>6</div>  
+      </div>   
+    </main>
+  )
+}
