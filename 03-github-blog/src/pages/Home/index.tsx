f7ddf551a3b4ba624ce14b509e65e9586b719f45
Guilherme <gibbon309@hotmail.com>
2024-08-20 18:32:21 -0300

    add style search

--- 03-github-blog/src/pages/Home/index.tsx
+++ 03-github-blog/src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { HomeContainer, ProfileContainer } from "./styles";
+import { HomeContainer, LinkGithub, PostsContainer, ProfileContainer, SearchContainer } from "./styles";
 import { useState, useEffect } from "react";
 
 interface TypesProfile {
@@ -35,32 +35,39 @@ export function Home() {
           <img src={profile.avatar} alt="" />
           <div>
             <h2>{profile.name}</h2>
+            
+            <LinkGithub href="#">GITHUB</LinkGithub>
+            
             <p>
               Tristique volutpat pulvinar vel massa, 
               pellentesque egestas. Eu viverra massa quam dignissim
               aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.
             </p>
+
             <nav>
               <a href="#">{profile.login}</a>
-              <a href="#">{profile.followers}</a>
+              <a href="#">{profile.followers} seguidores</a>
             </nav>
           </div>
         </ProfileContainer>
       }
 
-      <div>
-        <h4>Publicações</h4>  
+      <SearchContainer>
+        <div>
+          <h4>Publicações</h4>  
+          <span>0 publicações</span>
+        </div>
         <input type="text" placeholder="Buscar conteúdo"/>
-      </div>
+      </SearchContainer>
 
-      <div>
+      <PostsContainer>
         <div>1</div>  
         <div>2</div>  
         <div>3</div>  
         <div>4</div>  
         <div>5</div>  
         <div>6</div>  
-      </div>   
+      </PostsContainer>   
     </HomeContainer>
   )
 }
--- 03-github-blog/src/pages/Home/styles.ts
+++ 03-github-blog/src/pages/Home/styles.ts
@@ -5,18 +5,105 @@ export const HomeContainer = styled.main`
   flex-direction: column;
   align-items: center;
   
+  margin-top: -80px;
 `
 
 export const ProfileContainer = styled.div`
-  min-width: 54rem;
-  min-height: 13.25rem;
+  width: 54rem;
+  height: 13.25rem;
+  border-radius: 10px;
+  padding: 2rem;
+  
+  position: relative;
+  z-index: 10;
   
   display: flex;
+  gap: 2rem;
   background-color: ${props => props.theme["base-profile"]};
 
   img {
     max-width: 9.25rem;
     max-height: 9.25rem;
     border-radius: 8px;
+
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    line-height: 130%;
+  }
+
+  p {
+    font-size: 1rem;
+    line-height: 160%;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    nav {
+      display: flex;
+      gap: 1.25rem;
+
+      a {
+        line-height: 160%;
+        text-decoration: none;
+        color: ${props => props.theme["base-subtitle"]};
+      }
+    }
   }
 `
+
+export const SearchContainer = styled.div`
+  width: 54rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+   }
+
+  h4 {
+    font-size: 1.125rem;
+  }
+
+  span {
+    font-size: 0.875rem;
+    line-height: 160%;
+    text-align: right;
+    color: ${props => props.theme["base-span"]}
+  }
+
+  input {
+    width: 100%;
+    border-radius: 6px;
+    padding: 0.75rem 1rem;
+
+    color: ${props => props.theme["base-text"]};
+    background-color: ${props => props.theme["base-input"]};
+    border:1px solid ${props => props.theme["base-border"]};
+
+    &::placeholder {
+      color: ${props => props.theme["base-label"]};
+    }
+  }
+`
+
+export const LinkGithub = styled.a`
+  position: absolute;
+  top: 2rem;
+  right: 2rem;
+
+  font-size: 0.75rem;
+  color: ${props => props.theme.blue};
+  text-decoration: none;
+  line-height: 160%;
+`
+
+export const PostsContainer = styled.div`
+  color: #fff
+`;
